cee675b49fb37b32bfaf9ee3c2464dcc59eb7ef9
Sim Boon Long <simboonlong@gmail.com>
2023-09-19 12:26:32 +0800

    feat: alpha release

---
+++ src/alpha.ts
@@ -0,0 +1,3 @@
+export const alpha = () => {
+  console.log("alpha!");
+};
--- src/foobar.ts
+++ src/foobar.ts
@@ -1,3 +1,6 @@
+import { alpha } from "./alpha";
+
 export const foobar = () => {
+  alpha();
   console.log("foobar!");
 };
